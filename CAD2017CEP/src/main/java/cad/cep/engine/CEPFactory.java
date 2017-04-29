c615557abb58afa965c5b6a9d97acf9265ca54e8
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-04-29 13:55:08 +0200

    cleaned test classes/methods

--- CAD2017CEP/src/main/java/cad/cep/engine/CEPFactory.java
+++ CAD2017CEP/src/main/java/cad/cep/engine/CEPFactory.java
@@ -1,21 +1,54 @@
 package cad.cep.engine;
 
-import java.util.Map;
-
 import cad.cep.database.DatabaseStub;
 import cad.cep.model.JSONMessage;
 
 public class CEPFactory {
-	
+
 	public static EsperService createNewService(){
 		EsperService service = new EsperService();
 		service.registerAdditionalEvent(JSONMessage.class);
+		addUpdateEvent(service);
+		addWarning(service);
+		addContext(service);
+		return service;
+	}
+	private static void addUpdateEvent(EsperService service) {
 		String statementQuery ="select * from JSONMessage";
 		service.createStatement(statementQuery, (newData, oldData) ->{
-			DatabaseStub stub = new DatabaseStub();
-			stub.saveMessage((JSONMessage)newData[0].getUnderlying());
+			for (int i = 0; i < oldData.length; i++) {
+				DatabaseStub stub = new DatabaseStub();
+				stub.saveMessage((JSONMessage)newData[0].getUnderlying());
+			}
 		});
-		return service;
+	}
+	private static void addWarning(EsperService service) {
+		String statement = "select temperature, location from JSONMessage"
+				+" where temperature <= 0";
+		service.createStatement(statement, (newData, oldData)->{
+			for (int i = 0; i < newData.length; i++) {
+				System.out.println("TEMPERATURE WARNING:");
+				System.out.println(newData[i].get("temperature"));
+				System.out.println("in " + newData[i].get("location"));
+
+			}
+		});
+	}
+	private static void addContext(EsperService service) {
+		//collects every Message and part them by location
+		String context = "create context TemperatureByLocation partition by location from JSONMessage";
+		//gets every locaition where the temperature drops under 0 within 2 hours
+		String contextStatement = "context TemperatureByLocation select * from pattern["
+				+ "every a=JSONMessage(temperature >=0) ->b=JSONMessage(temperature <0) where timer:within(120 minutes)]";
+		service.createStatementWithoutListener(context);
+		//reads a and b and prints the values
+		service.createStatement(contextStatement, (newData, oldData)->{
+			for (int i = 0; i < newData.length; i++) {
+				System.out.println("TEMPERATURE FALL");
+				System.out.println(newData[i].get("a"));
+				System.out.println(newData[i].get("b"));
+			}
+		});
 	}
 	//every other part is like a sql statement
 	public static String createWindow(boolean timeBased, String value){
--- CAD2017CEP/src/main/java/cad/cep/milf/InformationLifeFlow.java
+++ CAD2017CEP/src/main/java/cad/cep/milf/InformationLifeFlow.java
@@ -9,33 +9,21 @@ public class InformationLifeFlow {
 	public static void main(String[] args) {
 		EsperService service = CEPFactory.createNewService();
 		JSONMessage messageEvent = new JSONMessage();
-		String statement = "select temperature, location from JSONMessage"
-		+" where temperature <= 0";
-		System.out.println(statement);
-		service.createStatement(statement, (newData, oldData)->{
-			System.out.println("TEMPERATURE WARNING:");
-			System.out.println(newData[0].get("temperature"));
-			System.out.println("in " + newData[0].get("location"));
-		});
 		messageEvent.setLocation("Radolfzell");
 		messageEvent.setTemperature(-16);
-		//collects every Message and part them by location
-		String context = "create context TemperatureByLocation partition by location from JSONMessage";
-		//gets every locaition where the temperature drops under 0 within 2 hours
-		String contextStatement = "context TemperatureByLocation select * from pattern["
-				+ "every a=JSONMessage(temperature >=0) ->b=JSONMessage(temperature <0) where timer:within(120 minutes)]";
-		service.createStatementWithoutListener(context);
-		//reads a and b and prints the values
-		service.createStatement(contextStatement, (newData, oldData)->{
-			System.out.println("TEMPERATURE FALL");
-			System.out.println(newData[0].get("a"));
-			System.out.println(newData[0].get("b"));
-		});
 		JSONMessage message = new JSONMessage();
 		message.setLocation("Radolfzell");
 		message.setTemperature(17);
+		JSONMessage knMessage = new JSONMessage();
+		knMessage.setLocation("Konstanz");
+		knMessage.setTemperature(35);
+		service.sendEvent(knMessage);
 		service.sendEvent(message);
 		service.sendEvent(messageEvent);
+		JSONMessage newKN = new JSONMessage();
+		newKN.setLocation("Konstanz");
+		newKN.setTemperature(-100);
+		service.sendEvent(newKN);
 		
 	}
 
