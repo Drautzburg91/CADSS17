96b5cee5051835f57f0d8963429a54c63d8b0f09
Paul Drautzburg <pauldrautzburg@yahoo.com>
2017-05-16 21:45:55 +0200

    Initial Version for weatherdata daily

---
+++ mqtt/src/main/java/Mqtt/mqtt/WeatherData.java
@@ -0,0 +1,88 @@
+package Mqtt.mqtt;
+
+public class WeatherData {
+
+	double longitude;
+	double latitude;
+	String cityName;
+	String currentWeather;
+	int pressure;
+	int humitidy;
+	double windspeed;
+	int windDeg;
+	double temperature;
+	double temperatureMax;
+	double temperatureMin;
+	
+	
+	public int getWindDeg() {
+		return windDeg;
+	}
+	public void setWindDeg(int windDeg) {
+		this.windDeg = windDeg;
+	}
+
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+	public int getPressure() {
+		return pressure;
+	}
+	public void setPressure(int pressure) {
+		this.pressure = pressure;
+	}
+	public int getHumitidy() {
+		return humitidy;
+	}
+	public void setHumitidy(int humitidy) {
+		this.humitidy = humitidy;
+	}
+	public double getWindspeed() {
+		return windspeed;
+	}
+	public void setWindspeed(double windspeed) {
+		this.windspeed = windspeed;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	public double getTemperatureMax() {
+		return temperatureMax;
+	}
+	public void setTemperatureMax(double temperatureMax) {
+		this.temperatureMax = temperatureMax;
+	}
+	public double getTemperatureMin() {
+		return temperatureMin;
+	}
+	public void setTemperatureMin(double temperatureMin) {
+		this.temperatureMin = temperatureMin;
+	}
+	public String getCurrentWeather() {
+		return currentWeather;
+	}
+	public void setCurrentWeather(String currentWeather) {
+		this.currentWeather = currentWeather;
+	}
+	
+}
